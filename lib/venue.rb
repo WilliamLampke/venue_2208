161d75ca6aead1ad24a26267d5cb40111e561f36
William Lampke <gravitybangers@gmail.com>
2022-08-24 12:50:17 -0400

    add all iterations

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,35 @@
 class Venue
+    attr_reader :name, :capacity, :patrons
+    
+    def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+    end
+    
+    def add_patron(name)
+        @patrons.push(name)
+    end
+
+    def yell_at_patrons
+        capsnames = []
+        @patrons.each do | patron |
+            capsnames.push(patron.upcase)
+        end
+        p capsnames
+    end
+
+    def over_capacity
+        if @patrons.length > @capacity
+            p true
+        else
+            p false
+    
+        end
+    end
+
+    def kick_out
+        @patrons.pop
+    end
 end
+
